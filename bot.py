0858db93084c44ab4527e194bbae21350c2a208b
shihkebab <tg: @shihkebab>
2023-02-24 01:23:09 +0300

    Added menu button registration / Добавлена регистрация кнопки меню

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import user_handlers, other_handlers
+from keyboards.set_menu import set_menu_commands
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
@@ -26,6 +27,9 @@ async def main():
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
+
+    # Настраиваем кнопку menu
+    await set_menu_commands(bot)
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user_handlers.router)
